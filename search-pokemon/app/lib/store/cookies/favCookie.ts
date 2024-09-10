9cfb6d053f2c98b7d304d0ffe28adfba48f06352
Lwin Maung Maung <lwinmaungmaung.ytu@gmail.com>
2024-09-10 15:47:08 +0630

    Fixes Cookie Path

--- search-pokemon/app/lib/store/cookies/favCookie.ts
+++ search-pokemon/app/lib/store/cookies/favCookie.ts
@@ -3,7 +3,9 @@ import Cookies from "universal-cookie";
 const favCookieName = "FAV-COOKIE"
 
 export function setFavCookie(value: Array<string>, cookies: Cookies){
-    cookies.set(favCookieName,  value);
+    cookies.set(favCookieName,  value, {
+        path: "/"
+    });
 }
 
 export function getFavCookie(cookies: Cookies): Array<string>{
